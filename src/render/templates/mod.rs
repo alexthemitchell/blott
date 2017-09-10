0bddec4482436fc33d10a4e6a92419767972ae52
Alex Mitchell <alex@alexthemitchell.com>
2017-09-10 15:08:31 -0700

    Use Rust string instead of HTML file for template base

--- src/render/templates/mod.rs
+++ src/render/templates/mod.rs
@@ -1,30 +1,16 @@
-use std::fs::File;
+use std::fs::{self,File};
 use std::io::{self,Read};
 use std::path::PathBuf;
 
-static POST_TEMPLATE_PATH: &'static str = "post.html";
+mod post;
 
 pub enum Template {
     Post,
 }
 
-fn path_for_template(template: Template) -> PathBuf {
-    let mut path = PathBuf::new();
-    match template {
-        Post  => path.push(POST_TEMPLATE_PATH),
-    }
-    path
-}
-
 pub fn get_template(template: Template) -> String {
-    let template_path = path_for_template(template);
-    let mut template_file = File::open(template_path).unwrap();
-    let mut template = String::new();
-    template_file.read_to_string(&mut template);
-    template
+    match template {
+        Post => post::Template.to_string(),
+    }
 }
 
-#[test]
-fn post_template_exists() {
-    assert!(fs::canonicalize(POST_TEMPLATE)).is_ok();
-}
--- src/render/templates/post.rs
+++ src/render/templates/post.rs
@@ -1,8 +1,8 @@
-<html>
+pub static Template: &'static str = "<html>
   <head>
     <title>{{ title }} | blott</title>
   </head>
   <body>
     {{ content }}
   </body>
-</html>
+</html>";
