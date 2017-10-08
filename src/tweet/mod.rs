b2689ec7d9f65c45b1e4dcdbadff8722636a3e3b
Alex Mitchell <alex@alexthemitchell.com>
2017-10-08 16:06:12 -0700

    Add function I realized was not useful

--- src/tweet/mod.rs
+++ src/tweet/mod.rs
@@ -2,7 +2,17 @@ extern crate egg_mode;
 
 mod private_strings;
 
-use self::egg_mode::tweet::*;
+use self::egg_mode::{ tweet,search,error };
+
+use std::result;
+
+type Result<T> = result::Result<T, TweetError>;
+
+#[derive(Debug)]
+pub enum TweetError {
+    Send(error::Error),
+    Fetch(error::Error),
+}
 
 fn get_token() -> egg_mode::Token<'static> {
     let con_token = egg_mode::KeyPair::new(
@@ -19,7 +29,20 @@ fn get_token() -> egg_mode::Token<'static> {
     }
 }
 
-pub fn tweet(message: &str) -> Result<egg_mode::Response<Tweet>, egg_mode::error::Error> {
-    let draft = DraftTweet::new(message);
-    draft.send(&get_token())
+pub fn tweet(message: &str) -> Result<egg_mode::Response<tweet::Tweet>> {
+    let draft = tweet::DraftTweet::new(message);
+    match draft.send(&get_token()) {
+        Ok(response)    => Ok(response),
+        Err(e)          => Err(TweetError::Send(e))
+    }
+}
+
+pub fn get_tweets(search_string: &str) -> Result<Vec<tweet::Tweet>> {
+    let search = search::search(search_string)
+                         .result_type(search::ResultType::Recent)
+                         .call(&get_token());
+    match search {
+        Ok(response) => Ok(response.statuses.clone()),
+        Err(e)     => Err(TweetError::Fetch(e)),
+    }
 }
